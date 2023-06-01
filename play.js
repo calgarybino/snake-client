20e08b732a2b9c5b0afe27e86c495e6ef3768ebe
calgarybino <binoandemeskel1234@gmail.com>
2023-06-01 19:50:34 -0400

    Add data handling and logging to connect function

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.25.49.138", // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("Incoming data:", data);
+    // Process the incoming data here if needed
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
