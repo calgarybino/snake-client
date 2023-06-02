187df4abd23b0272c07f3860c2d0982fcc74490a
calgarybino <binoandemeskel1234@gmail.com>
2023-06-01 22:41:31 -0400

    Update constants.js and client.js with IP and PORT values

--- client.js
+++ client.js
@@ -1,26 +1,22 @@
-const net = require("net");
+const net = require('net');
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "172.25.49.138", // IP address here,
-    port: 50541, // PORT number here,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // event listener for incoming data
-  conn.on("data", (data) => {
-    console.log("Incoming data:", data);
-    // Process the incoming data here if needed
-  });
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: bin");
-    //conn.write("Move: up");
-  });
-
   return conn;
 };
 
-module.exports = connect;
+console.log("Connecting ...");
+connect();
+
+
+
+
+
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = "172.25.49.138"; // Replace with the actual IP address
+const PORT = 50541; // Replace with the actual PORT number
+
+module.exports = {
+  IP,
+  PORT
+};
