e31379828c2571dda537f1e6f3da01e58524b8d7
calgarybino <binoandemeskel1234@gmail.com>
2023-06-01 20:58:46 -0400

    Update play.js with client and input modules

--- play.js
+++ play.js
@@ -1,18 +1,7 @@
-const connect = require("./client.js");
-const handleUserInput = (key) => {
-  // Exiting the game on pressing the "Ctrl + C" key combination
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
 console.log("Connecting ...");
 connect();
+
 setupInput();
