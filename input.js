9793f367c754654682909ceb603599ecc7164b68
calgarybino <binoandemeskel1234@gmail.com>
2023-06-01 22:26:17 -0400

    Update input.js to handle canned messages

--- input.js
+++ input.js
@@ -1,7 +1,24 @@
-let connection;
+const handleUserInput = (key) => {
+  if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "d") {
+    connection.write("Move: right");
+  } else if (key === "1") {
+    connection.write("Say: Hello!");
+  } else if (key === "2") {
+    connection.write("Say: Howdy!");
+  } else if (key === "3") {
+    connection.write("Say: Cheers!");
+  } else if (key === "4") {
+    connection.write("Say: GG!");
+  }
 
-const setupInput = (conn) => {
-  connection = conn;
+  // Example: Exiting the game on pressing the "Ctrl + C" key combination
+  if (key === "\u0003") {
+    process.exit();
+  }
 };
-
-module.exports = { setupInput };
