17873c04bc5cc6d9709a1db42be04406e60c6e71
calgarybino <binoandemeskel1234@gmail.com>
2023-06-29 22:22:45 -0400

    created connect func and exporting the func

--- client.js
+++ client.js
@@ -1,27 +1,16 @@
-const net = require("net");
-const { IP, PORT } = require("./constants");
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: IP, // IP address here,
-    port: PORT, // PORT number here,
+    host: "localhost", // IP address here,
+    port: 50541, // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
-    //conn.write("Say:Hello");
     console.log("Connected to server");
   });
-  conn.on("data", (data) => {
-    console.log("Received data from server:", data.toString());
-  });
-  conn.on("end", () => {
-    console.log("Disconnected from server");
-  });
-  conn.on("error", (err) => {
-    console.error("Error:", err);
-    // Your code here...
-  });
+
   return conn;
 };
-module.exports = { connect };
+module.exports={connect};
