c6c92e791f949a6567c97a11d5387ac85feb96a6
calgarybino <binoandemeskel1234@gmail.com>
2023-06-29 22:19:46 -0400

    moved the connection fun to play.js and importing from play.js

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connected to server");
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
